67a9b57ff21d5036e457314f3f71e12813156b41
selim1 <mohamedselim767@gmail.com>
2025-12-03 00:00:00 +0200

    SomeUpdate

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\HoldController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\HoldController;
 use App\Http\Controllers\Api\OrderController; 
-use App\Http\Controllers\PaymentWebhookController;
+use App\Http\Controllers\Api\PaymentWebhookController;
 
 Route::prefix('api')->group(function () {
     Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::post('/products', [ProductController::class, 'store']); 
+    
+    // Hold routes
     Route::post('/holds', [HoldController::class, 'store']);
-    Route::post('/orders', [OrderController::class, 'store']); 
+    
+    // Order routes (Note: OrderController is in Api namespace)
+    Route::post('/orders', [OrderController::class, 'store']);
+    
+    // Payment webhook
     Route::post('/payments/webhook', [PaymentWebhookController::class, 'handle']);
 });
